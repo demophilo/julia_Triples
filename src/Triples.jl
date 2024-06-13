1646091a5fcdfb648972b0a804842861d6411f41
Karl Linek <karl@linek.at>
2024-06-13 23:12:54 +0200

    added function generate_triple

--- src/Triples.jl
+++ src/Triples.jl
@@ -1,5 +1,12 @@
 module Triples
+function generate_triple(big_num::Int, small_num::Int)
+    _a::Int = big_num^2 - small_num^2
+    _b::Int = big_num^2 + small_num^2
+    _c::Int = 2 * big_num * small_num
+    _triple = sort([_a, _b, _c])
+    return _triple
+    
+end
 
-greet() = print("Hello World!")
 
 end # module Triples
