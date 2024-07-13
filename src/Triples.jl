d59e82b61a256b82a5be451295270f83355586ce
shengkai xia <karl@linek.at>
2024-07-13 10:07:43 +0800

    added function add_angeles

--- src/Triples.jl
+++ src/Triples.jl
@@ -2,7 +2,7 @@ module Triples
 
 export generate_triples
 
-function generate_triple(big_num::Int, small_num::Int)
+function generate_pyt_triple(big_num::Int, small_num::Int)
     _a::Int = big_num^2 - small_num^2
     _b::Int = big_num^2 + small_num^2
     _c::Int = 2 * big_num * small_num
@@ -11,7 +11,7 @@ function generate_triple(big_num::Int, small_num::Int)
     
 end
 
-function generate_trojan_triples(big_num::Int, small_num::Int)::NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}
+function generate_trojan_triple(big_num::Int, small_num::Int)::NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}
     _a::Int = big_num^2 + small_num^2 - big_num * small_num
     _b::Int = abs(big_num^2 - 2 * big_num * small_num)
     _c::Int = abs(small_num^2 - 2 * big_num * small_num)
@@ -31,7 +31,7 @@ function get_trojan_triples(num::Int)
     _triples::Vector{NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}} = []
     for big_num::Int in 3:num
         for small_num::Int in 1:floor((big_num-1)/2)
-            _triple = generate_trojan_triples(big_num, small_num)
+            _triple = generate_trojan_triple(big_num, small_num)
             if _triple ∉ _triples
                 push!(_triples, _triple)
             end
@@ -41,22 +41,28 @@ function get_trojan_triples(num::Int)
     return _triples
 end
 
-triples = get_trojan_triples(20)
-for item in triples
-    println(item)
-end
 
 
 
-
-for big_num::Int in 3:20
-    for small_num::Int in 1:floor((big_num-1)/2)
-        a,b,c = generate_trojan_triples(big_num, small_num)
+function add_angles(triples::Vector{NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}})
+    
+    _ext_triples = []
+    for item in triples
+        a,b,c = item
         α = angle_by_cos_law(a, b, c)
         β = angle_by_cos_law(b, a, c)
         γ = angle_by_cos_law(c, a, b)
-        println("$big_num  $small_num  $a  $b  $c  $α   $β   $γ")
+        push!(_ext_triples, (a=a, b=b, c=c, α=α, β=β, γ=γ))
     end
+    return _ext_triples
+end
+
+triples = get_trojan_triples(20)
+ext_triples = add_angles(triples)
+
+for item in ext_triples
+    a,b,c,α,β,γ = item      
+    println("$a  $b  $c  $α   $β   $γ")
 end
 
 
