b883d24056e711579922a32ef9f2b6bd089e6f14
shengkai xia <karl@linek.at>
2024-07-16 19:38:36 +0800

    added test for function generate_trojan_triple_120

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,3 +9,10 @@ using .Triples
 	@test generate_pyt_triple(3, 1) != (a = 6, b = 8, c = 10)
 	@test generate_pyt_triple(3, 2) == (a = 5, b = 12, c = 13)
 end
+
+@testset "generate_trojan_triple_120" begin
+	@test generate_trojan_triple_120(3, 1) == (a = 3, b = 5, c = 7)
+	@test generate_trojan_triple_120(6, 2) == (a = 3, b = 5, c = 7)
+	@test generate_trojan_triple_120(6, 2) != (a = 12, b = 20, c = 28)
+	@test generate_trojan_triple_120(5, 2) == (a = 5, b = 16, c = 19)
+end
