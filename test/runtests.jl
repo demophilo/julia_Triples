d58db47d588f87329d5da1ff98c7cc3693ab100e
shengkai xia <karl@linek.at>
2024-07-16 20:24:46 +0800

    added testset for function calc_angle_by_cos_law

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,3 +16,8 @@ end
 	@test generate_trojan_triple_120(6, 2) != (a = 12, b = 20, c = 28)
 	@test generate_trojan_triple_120(5, 2) == (a = 5, b = 16, c = 19)
 end
+@testset "calc_angle_by_cos_law" begin
+    @test calc_angle_by_cos_law(5, 4, 3) ≈ 90.0 atol = 1e-10
+    @test calc_angle_by_cos_law(7, 5, 3) ≈ 120.0 atol = 1e-10
+    @test calc_angle_by_cos_law(13, 12, 5) ≈ 90.0 atol = 1e-10
+end
