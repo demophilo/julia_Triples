c0cac3bff2358da62097a1fb8ce62b8985a3f4e0
shengkai xia <karl@linek.at>
2024-07-17 02:09:43 +0800

    added test for function generate_trojan_triple_vector

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,3 +21,9 @@ end
     @test T.calc_angle_by_cos_law(7, 5, 3) ≈ 120.0 atol = 1e-10
     @test T.calc_angle_by_cos_law(13, 12, 5) ≈ 90.0 atol = 1e-10
 end
+@testset "generate_trojan_triple_vector" begin
+    triple = T.generate_trojan_triple_vector(6)
+    @test length(triple) == 4
+    @test typeof(triple) == Vector{@NamedTuple{a::Int64, b::Int64, c::Int64}}
+    @test triple == [(a = 3, b = 5, c = 7), (a = 7, b = 8, c = 13), (a = 5, b = 16, c = 19), (a = 11, b = 24, c = 31)]
+end
