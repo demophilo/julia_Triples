0de195d5ef0ec554941474d4287595eae302c06d
shengkai xia <karl@linek.at>
2024-07-16 19:41:26 +0800

    added function generate_trojan_triple_120

--- src/module_Triples.jl
+++ src/module_Triples.jl
@@ -1,6 +1,6 @@
 module Triples
 
-export generate_pyt_triple
+export generate_pyt_triple, generate_trojan_triple_120
 
 """
 	generate_pyt_triple(big_num::Int, small_num::Int)::NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}
@@ -22,6 +22,30 @@ function generate_pyt_triple(big_num::Int, small_num::Int)::NamedTuple{(:a, :b, 
 	triple = sort([a, b, c])
 	return (a = triple[1], b = triple[2], c = triple[3])
 end
+
+
+"""
+	generate_trojan_triple_120(big_num::Int, small_num::Int)::NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}
+
+generates a trojan triple with one angle of 120°
+Input: two intergers
+Output: sorted named trojan triple
+"""
+function generate_trojan_triple_120(big_num::Int, small_num::Int)::NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}
+	a::Int = big_num^2 + small_num^2 - big_num * small_num
+	b::Int = abs(big_num^2 - 2 * big_num * small_num)
+	c::Int = abs(small_num^2 - 2 * big_num * small_num)
+	gcd_abc = foldl(gcd, [a, b, c])
+	if gcd_abc > 1
+		a ÷= gcd_abc
+		b ÷= gcd_abc
+		c ÷= gcd_abc
+	end
+	triple = sort([a, b, c])
+	return (a = triple[1], b = triple[2], c = triple[3])
+end
+
+
 
 
 
