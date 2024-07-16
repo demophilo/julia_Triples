744bdfeb7bb719aae199568ebf67fd6b1909e1a7
shengkai xia <karl@linek.at>
2024-07-16 17:19:40 +0800

    added testset for function generate_pyt_triple

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using Test
-using Triples
 
-@testset "Triples Tests" begin
-    @test Triples.generate_triples_test(1, 2) == [3, 4, 5]
+include("../src/Triples.jl")
+include("../src/module_Triples.jl")
+using .Triples
+@testset "Pythagorean triples generation Tests" begin
+	@test generate_pyt_triple(2, 1) == (a = 3, b = 4, c = 5)
+	@test generate_pyt_triple(3, 1) == (a = 3, b = 4, c = 5)
+	@test generate_pyt_triple(3, 1) != (a = 6, b = 8, c = 10)
+	@test generate_pyt_triple(3, 2) == (a = 5, b = 12, c = 13)
 end
