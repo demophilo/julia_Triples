7b61da2ea43a2ad8c7f3fe7856aaf9714420ea01
shengkai xia <karl@linek.at>
2024-07-17 09:16:25 +0800

    removed functions to module_Triples.jl

--- src/Triples.jl
+++ src/Triples.jl
@@ -1,14 +1,6 @@
 include("module_Triples.jl")
 import .Trojan as T
 
-#using StatsBase # Für countmap
-triple = T.get_ext_trojan_triple_vector(6)
-triple = T.add_angles_to_triple_vector(triple)
-println(triple)
-println(typeof(triple))
-#=
-"""
-	merge(a::NamedTuple, b::NamedTuple)
 
 
 
@@ -20,72 +12,13 @@ println(typeof(triple))
 
 
 
-"""
-	analyze_c_frequencies(ext_triples, max_num)
 
-analyzes the frequency of multiple c values of the triples up to a given number
-input: vector of triples, number
-Output: dictionary with the key of the frequency and the value of frequency the same number of c values
-"""
-function analyze_c_frequencies(ext_triples, max_num)
-	c_values = [item.c for item in ext_triples if item.c < max_num^2 * 3 / 4 + 1]
-	c_frequencies = countmap(c_values)
-	frequency_counts = Dict()
-	for freq in values(c_frequencies)
-		frequency_counts[freq] = get(frequency_counts, freq, 0) + 1
-	end
 
-	return frequency_counts
-end
 
-"""
-	get_trojan_triples_for_a_number(num::Int)::Vector{NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}}
 
-Input: number
-Output: vector of all possible named trojan triples, which have an edge of the size of the input number
-"""
-function get_trojan_triples_for_a_number(num::Int)::Vector{NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}}
-	big_num::Int = ceil(sqrt(4 * num / 3 + 1))
-	triple_vector = generate_trojan_triple_vector(big_num)
-	every_triple_vector = expand_trojan_triple_vector(triple_vector)
-	right_triple_set::Set{NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}} = Set[]
-	for item in every_triple_vector
-		for edge in item
-			if num % edge == 0
-				a = item.a * num / edge
-				b = item.b * num / edge
-				c = item.c * num / edge
-				triple = (a = a, b = b, c = c)
-				push!(right_triple_set, triple)
-			end
-		end
 
-	end
-	right_triple_vector::Vector{NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}} = Vector(every_triple_set)
-	sort!(right_triple_vector, by = x -> (x.c, x.b))
-	return right_triple_vector
-end
 
-"""
-	get_every_trojan_triple(triples::Vector{NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}})
 
-Input: vector of named trojan triples with one angle of 120°
-Output: vector of named trojan triples with all possible angles and the same hypothenuse
-"""
-function expand_trojan_triple_vector(triples::Vector{NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}})
-	every_triple_set::Set{NamedTuple{(:a, :b, :c), Tuple{Int, Int, Int}}} = []
-	for item in triples
-		first_triple = (a = item.a, b = item.c, c = item.a + item.b)
-		second_triple = (a = item.b, b = item.c, c = item.a + item.b)
-		push!(every_triple_set, first_triple)
-		push!(every_triple_set, second_triple)
-		push!(every_triple_set, item)
-	end
-	every_triple_vector = Vector(every_triple_set)
-	sort!(every_triple_vector, by = x -> (x.c, x.b))
-	return every_triple_vector
-end
-triples = get_trojan_triples_for_a_number(111)
-println(triples)
 
-=#
+
+
