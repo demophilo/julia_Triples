3084d4cc0c4923f669f322aaf21c065425ae4552
Karl Linek <karl@linek.at>
2024-12-07 00:09:11 +0100

    ???

--- src/Triples.jl
+++ src/Triples.jl
@@ -1,5 +1,5 @@
 module Triples
-export generate_triples()
+export generate_triples
 function generate_triple(big_num::Int, small_num::Int)
     _a::Int = big_num^2 - small_num^2
     _b::Int = big_num^2 + small_num^2
